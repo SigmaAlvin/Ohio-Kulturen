2ea693e2110556cc1a860af281578b8f0d6af4e7
SigmaAlvin <164363873+SigmaAlvin@users.noreply.github.com>
2024-05-30 10:04:54 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', (event) => 
+    {
+        const hr = document.getElementById('Streck');
+        setTimeout(() => {
+            hr.classList.add('övergång');
+        }, 100);
+    });
